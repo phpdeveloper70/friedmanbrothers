049273949bc74923d99e04c3f8da86a0bef786b4
priyankasinghdwarka <44695350+priyankasinghdwarka@users.noreply.github.com>
2018-11-28 19:12:03 +0530

    Update Product_model.php

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -40,5 +40,33 @@ class Product_model extends CI_Model
 		$this->db->order_by("id");
 	 return $this->db->get('departments')->result();
 	}
+ 	/*=====Added By priiyanka on 28 nov 18 for add whishlist============*/
+
+		public function check_wishlist_data($userid,$prodid)
+	{
+		$this->db->where('userid',$userid);
+		$this->db->where('prodid',$prodid);
+		return $this->db->get('favorites')->result();
+	}
+	public function save_wishlist_data($data)
+	{
+		$this->db->insert('favorites',$data);
+		return $this->db->insert_id();
+	}
+ 	
+ 	public function get_wishlist_data($userid)
+	{ 
+		$this->db->select("product.*, wish.id as wish_id, wish.userid, wish.prodid");
+		$this->db->from('favorites as wish');
+		$this->db->join('products as product','product.id = wish.prodid','inner');
+		$this->db->where('wish.userid',$userid);
+		return $this->db->get()->result();
+	}
+	
+	public function delete_wishlist($id)
+	{
+		$this->db->where('id',$id);
+		$this->db->delete('favorites');
+	}
  	
 }
